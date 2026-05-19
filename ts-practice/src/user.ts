05222f7207e8086b1c58bef8fa8f99a1460a3519
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-05-19 11:49:37 +0900

    TypeScript:Chapter1 ハンズオン 1

---
+++ ts-practice/src/user.ts
@@ -0,0 +1,21 @@
+interface User {
+    name: string;
+    age: number;
+    email?: string;
+}
+
+function formatUser(user: User): string {
+    let result = `名前: ${user.name}, 年齢: ${user.age},歳`;
+
+    if (user.email) {
+        result += `,メール: ${user.email}`;
+    }
+
+    return result;
+}
+
+const user1: User = { name: "田中太郎", age: 20 };
+const user2: User = { name: "佐藤花子", age: 30, email: "hanako@example.com" };
+
+console.log(formatUser(user1));
+console.log(formatUser(user2));
