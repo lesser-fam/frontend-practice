ab50a28a2824d72c5691ee50b2e74bdf0a87ae2e
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-05-21 10:58:06 +0900

    React Chapter2 ハンズオン

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Card from "./components/Card";
 import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
 
@@ -6,10 +7,23 @@ function App() {
         <>
             <WelcomeMessage />
 
-            {/* UserProfileコンポーネントを2回再利用 */}
-            <UserProfile name="田中太郎" hobby="プログラミング" />
-            <UserProfile name="高橋花子" hobby="ネットサーフィン" />
-            <UserProfile name="山田次郎" hobby="筋トレ" />
+            <Card>
+                <h2>自由なテキスト</h2>
+                <UserProfile name="田中太郎" age={25} hobby="プログラミング" />
+            </Card>
+
+            <Card>
+                <UserProfile
+                    name="高橋花子"
+                    hobby="ネットサーフィン"
+                    bio="プログラミング練習中"
+                />
+            </Card>
+
+            <Card>
+                <UserProfile name="山田次郎" age={35} hobby="筋トレ" />
+                <button>押せないボタン</button>
+            </Card>
         </>
     );
 }
--- my-react-app/src/components/UserProfile.tsx
+++ my-react-app/src/components/UserProfile.tsx
@@ -1,15 +1,24 @@
 type UserProfileProps = {
     name: string;
+    age?: number;
     hobby: string;
+    bio?: string;
 };
 
-function UserProfile({ name, hobby }: UserProfileProps) {
+function UserProfile({
+    name,
+    age,
+    hobby,
+    bio = "よろしくお願いします。",
+}: UserProfileProps) {
     return (
         // ルール1: 単一のルート要素で囲む
         // ルール3: classではなくclassNameを使う
-        <div className="user-profile">
+        <div>
             <p>名前: {name}</p>
+            {age && <p>年齢:{age}歳</p>}
             <p>趣味: {hobby}</p>
+            {bio && <p>自己紹介: {bio}</p>}
             {/* ルール2: 閉じタグを忘れない */}
             <hr />
         </div>
