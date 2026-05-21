42c9f52a92cfdd93db960f0b6f110ba92573e767
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-05-21 09:19:00 +0900

    React Chapter1 練習

---
+++ my-ts-project/src/App.js
@@ -0,0 +1,12 @@
+import Button from './Button'; // Buttonコンポーネントをインポート
+
+function App() {
+  return (
+    <div>
+      <h1>ようこそ、私のアプリへ！</h1>
+      <Button /> {/* Buttonコンポーネントを呼び出し */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-ts-project/src/Button.js
@@ -0,0 +1,6 @@
+function Button() {
+  return <button>私はボタンです</button>;
+}
+
+// 他のファイルで使えるようにエクスポート
+export default Button;
